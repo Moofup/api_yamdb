be1a2e7817963530587c947667dfb3375c48393b
Бакушин Михаил <moofus@inbox.ru>
2022-03-31 11:22:15 +0300

    Third review

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -15,7 +15,9 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True)
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username')
 
     class Meta:
         fields = '__all__'
